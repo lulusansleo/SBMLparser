28ab60c574a53145398009fa6b2bbc57063ac9af
lulusansleo <luan.bourbier@epitech.eu>
2023-06-09 10:39:36 +0200

    [rm] debug printf

--- src/error_handling.c
+++ src/error_handling.c
@@ -21,7 +21,6 @@ int error_handling(int ac, char **av)
     for (int i = 0; av[i]; i++) {
         if (strcmp(av[i], "-i") == 0 && !av[i + 1])
             return 1;
-        printf("%d\n", i);
         if (av[i][0] == '-' && !is_flag(av[i][1]))
             return 1;
     }
